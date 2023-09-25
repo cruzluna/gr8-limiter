66d1d8d8b0be92f552f0d1b310d86d2ae36e10af
Cruz <lunac8@unlv.nevada.edu>
2023-09-25 13:14:05 -0700

    cards hoverable in dashboard

--- frontend/src/app/(user)/dashboard/page.tsx
+++ frontend/src/app/(user)/dashboard/page.tsx
@@ -95,20 +95,20 @@ export default function Page() {
       <section>
         <div className="flex items-center justify-center pt-[1rem]">
           <div className="flex flex-row flex-grow-0 gap-x-4 w-full p-3">
-            <Card className="w-1/3 py-4" isPressable>
+            <Card className="w-1/3 py-4" isPressable isHoverable>
               <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                 <small className="text-default-500">12 Integrations</small>
                 <h4 className="font-bold text-large">Manage Integrations</h4>
               </CardHeader>
             </Card>
 
-            <Card className="w-1/3 py-4" isPressable>
+            <Card className="w-1/3 py-4" isPressable isHoverable>
               <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                 <small className="text-default-500">12 Endpoints</small>
                 <h4 className="font-bold text-large">View Metrics</h4>
               </CardHeader>
             </Card>
-            <Card className="w-1/3 py-4" isPressable>
+            <Card className="w-1/3 py-4" isPressable isHoverable>
               <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                 <small className="text-default-500">username</small>
                 <h4 className="font-bold text-large">Manage Profile</h4>
