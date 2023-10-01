a5d2996e1985719cd4129b43e77a85ee8a5963a1
Cruz <lunac8@unlv.nevada.edu>
2023-09-30 18:47:06 -0700

    initial routing config

---
+++ backend/api/router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/KnlnKS/gr8-limiter/services/gr8-limiter/api/handlers"
+)
+
+func PublicRoutes(router *fiber.App) {
+	api := router.Group("/api")
+	v1 := api.Group("/v1")
+
+	// api/v1/ratelimit
+	v1.Post("/ratelimit", handlers.HandleRateLimit)
+
+	router.Use(func(c *fiber.Ctx) error {
+		return c.Status(404).SendString("Not found.")
+	})
+}
