def443b1f392c3a8c54c72a869dcca5860c2f018
Cruz <lunac8@unlv.nevada.edu>
2023-09-30 18:49:45 -0700

    🧨 initial routing config (#9)
    * initial routing config

--- backend/main.go
+++ backend/main.go
@@ -1,44 +1,21 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"math/rand"
-	"os"
-	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
 
-	"github.com/KnlnKS/gr8-limiter/services/gr8-limiter/internal/database"
+	"github.com/KnlnKS/gr8-limiter/services/gr8-limiter/api/router"
 )
 
 func main() {
-	ctx := context.Background()
-
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
-	}
-
-	dbUrl := os.Getenv("DB_URL")
-	fmt.Println("DB URL: ", dbUrl)
-	db, err := database.StartDatabase(ctx, dbUrl)
-	if err != nil {
-		log.Panicln(err)
-	}
-
-	record := database.ApiTableRecord{
-		ApiKey: RandomString(11),
-		UserId: rand.Int31(),
-	}
-
-	dbErr := db.Insert(ctx, record)
-
-	if dbErr != nil {
-		log.Panicln(dbErr)
 	}
 
 	// os.Exit(1)
@@ -51,39 +28,13 @@ func main() {
 	})
 	defer rdb.Close()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	app.Use(recover.New())
 
-	app.Post("/api-key", func(c *fiber.Ctx) error {
-		// generate an api key with you desired config
-		return c.SendString("Here is your API Key 👋!" + RandomString(10))
-	})
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path} ${latency}\n",
+	}))
 
-	// 4 reqs per second
-	rc := NewRateLimterConfig(RandomString(10), 4, 1)
-
-	for i := 0; i < 20; i++ {
-		allowed := rc.rateLimit(ctx, rdb)
-		if allowed {
-			fmt.Printf("Request %d allowed.\n", i)
-		} else {
-			fmt.Printf("Request %d blocked.\n", i)
-		}
-
-		time.Sleep(100 * time.Millisecond)
-	}
+	router.PublicRoutes(app)
 
 	app.Listen(":3000")
-}
-
-// throwaway....just for testing
-func RandomString(n int) string {
-	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-
-	s := make([]rune, n)
-	for i := range s {
-		s[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(s)
 }
