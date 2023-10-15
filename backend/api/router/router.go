fb5baa01bf4c4b051079be625e86ff7b50082a04
Cruz <lunac8@unlv.nevada.edu>
2023-10-15 14:48:04 -0700

    remove backend delete route

--- backend/api/router/router.go
+++ backend/api/router/router.go
@@ -13,9 +13,6 @@ func PublicRoutes(router *fiber.App) {
 	// api/v1/ratelimit
 	v1.Post("/ratelimit", handlers.HandleRateLimit)
 
-	// api/v1/apikey
-	v1.Delete("/apikey", handlers.HandleDeleteApiKey)
-
 	router.Use(func(c *fiber.Ctx) error {
 		return c.Status(404).SendString("Not found.")
 	})
