2f3e257f759a3abfd27a114d48a494218a247632
Cruz <lunac8@unlv.nevada.edu>
2024-01-03 15:40:44 -0800

    restart :(

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,18 +1,12 @@
-use axum::{
-    extract::State,
-    http::{HeaderMap, StatusCode},
-    response::IntoResponse,
-    routing::post,
-    Router,
-};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, Router};
 
-use axum_macros::debug_handler;
+// use axum_macros::debug_handler;
 use bb8_redis::{bb8, RedisConnectionManager};
 
-#[derive(Clone)]
-pub struct AppState {
-    pool: bb8::Pool<RedisConnectionManager>,
-}
+// #[derive(Clone)]
+// pub struct AppState {
+//     pool: bb8::Pool<RedisConnectionManager>,
+// }
 
 #[tokio::main]
 async fn main() {
@@ -30,52 +24,18 @@ async fn main() {
 
     // let client = redis::Client::open("redis://127.0.0.1/").expect("Invalid connection URL");
 
-    let state = AppState { pool };
+    // let state = AppState { pool };
 
     let app = Router::new()
-        .route("/api/v1/ratelimit", post(post_rate_limit))
-        .fallback(handler_404)
-        .with_state(state);
+        .route("/api/v1/ratelimit", post(String::from("DEEZ")))
+        .fallback(handler_404);
+    // .with_state(state);
     // .with_state(state.clone());
 
     // run our app with hyper, listening on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
     // Ok(())
-}
-
-#[debug_handler]
-// TODO: Fix the return type
-async fn post_rate_limit(headers: HeaderMap, State(state): State<AppState>) -> impl IntoResponse {
-    // for (key, value) in headers.iter() {
-    //     println!("Header: {:?} - {:?}", key, value);
-    // }
-
-    let api_key = match headers.get("x-api-key") {
-        // TODO: Change the unwrap
-        Some(val) => val.to_str().map(|x| x.to_string()).unwrap(),
-        None => return (StatusCode::BAD_REQUEST, "Invalid api key."),
-    };
-
-    // Rate Limit config
-    let _rc = RateLimitConfig {
-        id: api_key,
-        limit: 5,
-        window_size: 1,
-    };
-
-    (StatusCode::TOO_MANY_REQUESTS, "Rate limited")
-
-    // match rate_limit(conn, &rc) {
-    //     Ok(should_rate_limit) => {
-    //         if should_rate_limit {
-    //             (StatusCode::TOO_MANY_REQUESTS, "Rate limited")
-    //         } else {
-    //             (StatusCode::OK, "Good.")
-    //         }
-    //     }
-    //     Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong."),
-    // }
 }
 
 async fn handler_404() -> impl IntoResponse {
@@ -87,26 +47,4 @@ struct RateLimitConfig {
     id: String,
     limit: i64,       // default 5
     window_size: i64, // default 1
-}
-
-/// .
-///
-/// # Errors
-///
-/// This function will return an error if .
-// sliding log algo. Rate limited returns true and not rate limited is false.
-async fn rate_limit(
-    redis_con: redis::Connection,
-    rc: &RateLimitConfig,
-) -> Result<bool, redis::RedisError> {
-    println!("Made it into rate_limt");
-    // MULTI
-    // ZREMRANGEBYSCORE {key} 0 {now - window_size}
-    // ZADD {key} {now} {now}
-    // ZCARD {key}
-    // EXPIRE {key} {window_size}
-    // EXEC
-
-    // not rate limited
-    Ok(false)
 }
